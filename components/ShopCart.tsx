887b5e3afe0742d00925cda811419fbb71a8a3cd
htnphu <phuhanld@gmail.com>
2022-10-24 10:11:24 +0700

    adjust tabsize

--- components/ShopCart.tsx
+++ components/ShopCart.tsx
@@ -3,13 +3,21 @@ import { cartItems } from "./CartStore";
 
 export default function ShopCart() {
   const $cartItems = useStore(cartItems);
-  return (<div>
-    {Object.values($cartItems).length ? (
-      <ul>
-        {Object.values($cartItems).map((item,index)=>{
-          return(<li key={index}>#{item.name}: {item.quantity}</li>)
-        })}
-      </ul>
-    ) : <p>Cart is empty!!</p>}
-  </div>)
+  return (
+    <div>
+      {Object.values($cartItems).length ? (
+        <ul>
+          {Object.values($cartItems).map((item, index) => {
+            return (
+              <li key={index}>
+                #{item.name}: {item.quantity}
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <p>Cart is empty!!</p>
+      )}
+    </div>
+  );
 }
