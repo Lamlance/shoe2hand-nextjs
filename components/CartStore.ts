3abb870c80cfdf7e3d76b9fad09275012e562695
lamlance <lamhoangdien113@gmail.com>
2022-11-22 16:26:48 +0700

    Add to cart now need quantity

--- components/CartStore.ts
+++ components/CartStore.ts
@@ -9,17 +9,17 @@ export type CartItem = {
 export const isCartOpen = atom(false);
 export const cartItems = map<Record<string, CartItem>>({});
 
-export type ItemDisplayInfo = Pick<CartItem, "name" | "id">;
+export type ItemDisplayInfo = Pick<CartItem, "name" | "id" | "quantity">;
 
-export function addCartItem({ name, id }: ItemDisplayInfo) {
+export function addCartItem({ name, id ,quantity}: ItemDisplayInfo,replaceFlag:boolean = false) {
   isCartOpen.set(true);
   const existingEntry = cartItems.get()[name];
   if (existingEntry) {
     cartItems.setKey(name, {
       ...existingEntry,
-      quantity: existingEntry.quantity + 1,
+      quantity: (replaceFlag) ? quantity : (existingEntry.quantity + quantity),
     });
   } else {
-    cartItems.setKey(id, { id, name, quantity: 1 });
+    cartItems.setKey(id, { id, name, quantity: quantity });
   }
 }
